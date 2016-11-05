059be9a21cb41ebf605566c6d1f4ba15a625af41
Maayan Glikser <maayan@glikm.com>
2016-11-05 21:12:50 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const SocketCluster = require('socketcluster').SocketCluster;
+
+const args = process.argv.slice(2);
+let port = 5000;
+
+if (args[1] && args[0] === '-p' || args[0] === '--port') {
+  port = parseInt(args[1]);
+}
+
+new SocketCluster({
+  workers: 1,
+  brokers: 0,
+  port,
+  appName: 'bdsm',
+  wsEngine: 'uws',
+  workerController: __dirname + '/worker.js',
+  rebootWorkerOnCrash: true
+});
