ba6ed16f7cd97199c2ccc8a525b079b5f56ef3fa
Maayan Glikser <maayan@glikm.com>
2017-11-19 23:51:05 +0200

    Update socket cluster

--- worker.js
+++ worker.js
@@ -1,29 +1,30 @@
-var fs = require('fs');
-var express = require('express');
 var serveStatic = require('serve-static');
-var path = require('path');
+var path        = require('path');
+var SCWorker    = require('socketcluster/scworker');
 
-module.exports.run = function (worker) {
-  var app = require('express')();
+class Worker extends SCWorker {
+  run() {
+    var app = require('express')();
 
-  var httpServer = worker.httpServer;
-  var scServer = worker.scServer;
+    var httpServer = this.httpServer;
+    var scServer   = this.scServer;
 
-  app.set('view engine', 'ejs');
-  app.use(serveStatic(path.resolve(__dirname, 'node_modules', 'mimic', 'dist')));
+    app.set('view engine', 'ejs');
+    app.use(serveStatic(path.resolve(__dirname, 'node_modules', 'mimic', 'dist')));
 
-  app.get('/', function(req, res) {
-    const indexPath = path.resolve(__dirname, 'views', 'index');
-    res.render(indexPath, { port: worker.options.port });
-  });
-
-  httpServer.on('request', app);
-
-  scServer.on('connection', function (socket) {
-
-    socket.on('mimic-message', (message) => {
-      scServer.exchange.publish('mimic-message', message);
+    app.get('/', (req, res) => {
+      const indexPath = path.resolve(__dirname, 'views', 'index');
+      res.render(indexPath, { port: this.options.port });
     });
 
-  });
-};
+    httpServer.on('request', app);
+
+    this.scServer.on('connection', (socket) => {
+      socket.on('mimic-message', (message) => {
+        scServer.exchange.publish('mimic-message', message);
+      });
+    });
+  }
+}
+
+new Worker();
